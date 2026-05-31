afc7d7a31f74a8f09cbcd45eda358748e4a1424f
thecolorfix <bethanyduffy@gmail.com>
2026-05-31 12:14:42 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
